2517bb7d495c06f1b3ba61b1ab0036bfb54e02b2
MARCUS HSU <mh3346@columbia.edu>
2014-08-12 16:48:45 -0400

    Able to parse html file

--- __main__.py
+++ __main__.py
@@ -1,10 +1,32 @@
+from HTMLParser import HTMLParser
+
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        # print "Encountered a start tag:", tag
+        if tag == 'h1':
+        	print "Encountered a start tag:", tag, attrs
+
+    def handle_endtag(self, tag):
+    	pass
+        #print "Encountered an end tag :", tag
+    def handle_data(self, data):
+    	pass
+        # print "Encountered some data  :", data
+
+
+
 # filter an article form html file to find the trending topic
 def main():
 	path = 'res/'
-	file_name = 'Tech and Internet Industry News - HuffPost Tech'
+	file_name = 'Miss Bumbum 2014 Contestants Hope To Have Brazil\'s Best Butt'
+	htmltxt = ''
 	with open(path+file_name+'.html', 'r') as reading_file:
-		print reading_file.readline()
-		print reading_file.readline()
+		htmltxt = reading_file.read()
+
+	# instantiate the parser and fed it some HTML
+	parser = MyHTMLParser()
+	parser.feed(htmltxt)
 
 if __name__ == '__main__' :
 	main()
--- res/Tech and Internet Industry News - HuffPost Tech_files/js(4).php
+++
@@ -1,4 +0,0 @@
-/* From: wfe1-dtc : 9534 */
-window.UNB={config:{referrer:document.referrer,theme:"light",minimumBrowserWidth:320,devId:"sa1quMeQRBVzK6Jb"}};(function(){var b=document.createElement("script");b.id="unb_script";b.type="text/javascript";b.async=true;b.src="http://o.aolcdn.com/os/aol/unb.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(b,a)})();
-
-/* From: wfe1-dtc : 9534 *//* 48874d035bd3a6feca5bf338da72c0f1b06468d0 */
