a762424714df0290e957126d1d877d5eeebcf0b1
MARCUS HSU <mh3346@columbia.edu>
2014-08-12 15:41:13 -0400

    reformat..

--- __main__.py
+++ __main__.py
@@ -1,6 +1,10 @@
 # filter an article form html file to find the trending topic
-if __name__ == '__main__' :
+def main():
 	path = 'res/'
 	file_name = 'Tech and Internet Industry News - HuffPost Tech'
 	with open(path+file_name+'.html', 'r') as reading_file:
-		print reading_file.read()
+		print reading_file.readline()
+		print reading_file.readline()
+
+if __name__ == '__main__' :
+	main()
