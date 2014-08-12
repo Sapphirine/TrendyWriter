d06aad9b21c284d4d9e577416b377779d64d0edd
MARCUS HSU <mh3346@columbia.edu>
2014-08-12 15:21:10 -0400

    Successfully Read a html file

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
-
-
-
+# filter an article form html file to find the trending topic
 if __name__ == '__main__' :
-	pass
+	path = 'res/'
+	file_name = 'Tech and Internet Industry News - HuffPost Tech'
+	with open(path+file_name+'.html', 'r') as reading_file:
+		print reading_file.read()
