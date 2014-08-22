a5b2341e391cbfa5dea92e8290f79b25b0a2a868
MARCUS HSU <mh3346@columbia.edu>
2014-08-22 10:09:03 -0400

    Able to get all the titles from Huffintonpost.com

--- fetchtitle_v2.py
+++ fetchtitle_v2.py
@@ -1,93 +1,135 @@
 import urllib
 from HTMLParser import HTMLParser
 
-# specify url to fetch
-# url = 'http://www.upworthy.com/'
-# save path and file name
-local = 'res/url_01.html'
-strore_path = 'topic/all_topic.txt'
+store_html = 'res/url_'
+store_homepage = 'res/home_.html'
+strore_path = 'topic/all_topics.txt'
+store_url = 'url/all_urls.txt'
 
 # create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
 
-    def __init__(self):
+    def __init__(self, get_url):
         # call super constructor
         HTMLParser.__init__(self)
+        self.get_url = get_url
         self.found_a  = False
+
         self.found_h1 = False
         self.found_h2 = False
         self.found_h3 = False
         self.found_h4 = False
         self.found_h5 = False
         self.found_h6 = False
+
+        self.found_li = False
+        
         
     def handle_starttag(self, tag, attrs):
         # print "Encountered a start tag:", tag, attrs
-        if tag == 'a':
-         	self.found_a = True
-     	if tag == 'h1':
-        	self.found_h2 = True
-        if tag == 'h2':
-        	self.found_h2 = True
-        if tag == 'h3':
-            self.found_h2 = True
-        if tag == 'h4':
-         	self.found_h3 = True
-     	if tag == 'h5':
-         	self.found_h3 = True
-     	if tag == 'h6':
-         	self.found_h3 = True
+        if self.get_url == False:
+            if tag == 'a':
+                 self.found_a = True
+            if tag == 'h1':
+                self.found_h1 = True
+            if tag == 'h2':
+                self.found_h2 = True
+            if tag == 'h3':
+                self.found_h3 = True
+            if tag == 'h4':
+                 self.found_h4 = True
+            if tag == 'h5':
+                 self.found_h5 = True
+            if tag == 'h6':
+                 self.found_h6 = True
+        else:
+            if tag == 'li':
+                self.found_li = True
+            if tag == 'a' and self.found_li == True:
+                self.found_a = True
+                if attrs[0][1][0:11] == 'http://www.':
+                    # print 'href = ', attrs[0][1]
+                    with open(store_url, 'a+') as writing_file:
+                        writing_file.write(attrs[0][1] + '\n');
 
 
     def handle_endtag(self, tag):
-    	#print "Encountered an end tag :", tag
-    	if tag == 'a':
-         	self.found_a = False
-     	if tag == 'h1':
-        	self.found_h2 = False
-        if tag == 'h2':
-        	self.found_h2 = False
-        if tag == 'h3':
-            self.found_h2 = False
-        if tag == 'h4':
-         	self.found_h3 = False
-     	if tag == 'h5':
-         	self.found_h3 = False
-     	if tag == 'h6':
-         	self.found_h3 = False
+        #print "Encountered an end tag :", tag
+        if self.get_url == False:
+            if tag == 'a':
+                 self.found_a = False
+            if tag == 'h1':
+                self.found_h1 = False
+            if tag == 'h2':
+                self.found_h2 = False
+            if tag == 'h3':
+                self.found_h3 = False
+            if tag == 'h4':
+                self.found_h4 = False
+            if tag == 'h5':
+                self.found_h5 = False
+            if tag == 'h6':
+                self.found_h6 = False
+        else:
+            if tag == 'li':
+                self.found_li = False
+            if tag == 'a':
+                self.found_a = False
         
     def handle_data(self, data):
-    	# print "Encountered some data  :", data
-        if (self.found_h1 == True and self.found_a == True)\
-      	   or (self.found_h2 == True and self.found_a == True)\
-      	   or (self.found_h3 == True and self.found_a == True)\
-      	   or (self.found_h4 == True and self.found_a == True)\
-      	   or (self.found_h5 == True and self.found_a == True)\
-      	   or (self.found_h6 == True and self.found_a == True):
-            print 'Data = ', data 
-
-            # TODO..
-            #data.join()
-            with open(strore_path, 'a+') as writing_file:
-                writing_file.write(data + '\n');
+        # print "Encountered some data  :", data
+        if self.get_url == False:
+            if (self.found_h1 == True and self.found_a == True)\
+                 or (self.found_h2 == True and self.found_a == True)\
+                 or (self.found_h3 == True and self.found_a == True)\
+                 or (self.found_h4 == True and self.found_a == True)\
+                 or (self.found_h5 == True and self.found_a == True)\
+                 or (self.found_h6 == True and self.found_a == True):
+                print 'Data = ', data 
+                with open(strore_path, 'a+') as writing_file:
+                    writing_file.write(data + '\n');
+                 # TODO..
+                #data.join()
+        else:
+            if self.found_li == True and self.found_a == True:
+                # print 'Link = ', data
+                pass
 
     def handle_comment(self, data):
-        pass
+        if self.get_url == False:
+            pass
+        else:
+            pass
 
 
 def main():
-	url = raw_input('Enter a url : ')
-	result = urllib.urlretrieve(url, local)
-	# print result
-	htmltxt = ''
-	with open(local, 'r') as reading_file:
-		htmltxt = reading_file.read()
-		# print htmltxt
+    url = raw_input('Enter an url : ')
+    result = urllib.urlretrieve(url, store_homepage)
+    # print result
+    htmltxt = ''    
+    with open(store_homepage, 'r') as reading_file:
+        htmltxt = reading_file.read()
+        # print htmltxt
+    parser = MyHTMLParser(True)
+    parser.feed(htmltxt)
 
-	# instantiate the parser and fed it some HTML
-	parser = MyHTMLParser()
-	parser.feed(htmltxt)
+    urls = []
+    with open(store_url, 'r') as reading_file:
+        for line in reading_file:
+            urls.append(line)
+
+    for i in range(0, len(urls)):
+        store_html_path = store_html + '0' + str(i) + '.html'
+        result2 = urllib.urlretrieve(urls[i], store_html_path)
+        print result2
+        htmltxt2 = ''
+        with open(store_html_path, 'r') as reading_file:
+            htmltxt2 = reading_file.read()
+            # print htmltxt
+
+        parser = MyHTMLParser(False)
+        parser.feed(htmltxt2)
 
 if __name__ == '__main__' :
-	main()
+    main()
 
