c1599db715cbc7e146498280824b6d15e8b86a58
MARCUS HSU <mh3346@columbia.edu>
2014-11-28 13:09:46 -0500

    Finished crawl paragraph programmatic, and mahout clustering local

--- fetchtitle_v2.py
+++ fetchtitle_v2.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python2.6
 
-"""@package default
+""" @package default
 [TODO]
-(1) Filter the data that contains html original text(i.e. html tag),
-using formal way.
-(2) To process large-scale data efficiently, it is necessary.
-to generate the hashcode of all urls and titles, and "mod 1024" to
-separate a number of buckets, in order to eliminate duplicate urls
-and titles.
+(1) Filter the data that contains html original text(i.e. html tag), using formal way.
+(2) To process large-scale data efficiently:
+To generate the hashCode of all urls and titles, and "mod 1024" to separate into 1024 buckets, and avoid duplicate
+collision of data as much as possible.
 (3) Retrieve data from Ajax as well.
 (4) Retrieve data inside the tag <i> and <b>
 
-More details.
+More details..
 """
 
 """
@@ -32,7 +30,7 @@ store_topic = 'topic/all_topics.txt'
 store_url = 'url/all_urls.txt'
 
 # debug variables:
-DEBUG = True
+DEBUG = False
 debug_url = 'http://www.c-span.org/'
 debug_storepath = 'debug/debug_page.html'
 
@@ -45,16 +43,24 @@ def welcome_text():
     print "--------------------------------------------------------------"
     print "[DEVELOPER MODE]"
     print "Step 1: python fetchtitle_v2.py | tee debug.txt"
-    print "Step 2: (only if necessary) grep 'ERROR' debug.txt > error.txt"
+    print "Step 2: (IF NECESSARY) grep 'ERROR' debug.txt > error.txt"
     print "--------------------------------------------------------------"
 
+class CrawlTitlesAndUrls(HTMLParser):
+    """
+    MyHTMLParser class:
 
-# (note) Create a subclass and override the handler methods.
-class MyHTMLParser(HTMLParser):
+    Subclass HTMLParser, crawl urls and titles (support all character encoding) and eliminate all duplicates.
+    """
     url_list = []
     topic_list = []
 
     def __init__(self, get_url, url):
+        """ (self, bool, str)
+        MyHTMLParser class constructor:
+
+        :param get_url:  Determine whether crawl urls or titles.
+        """
         # call super constructor
         HTMLParser.__init__(self)
         self.get_url = get_url
@@ -98,11 +104,11 @@ class MyHTMLParser(HTMLParser):
                         if url[0:7] == 'http://' and url[-4:] != '.pdf' \
                                 and url[-4:] != '.asp':
                             # Eliminate duplicated url.
-                            if not url in MyHTMLParser.url_list:
+                            if not url in CrawlTitlesAndUrls.url_list:
                                 print '[INFO] href = ', url
                                 with open(store_url, 'a') as writing_file:
                                     writing_file.write(url + '\n')
-                                MyHTMLParser.url_list.append(url + '/')
+                                CrawlTitlesAndUrls.url_list.append(url + '/')
                             else:
                                 print '[DUPLICATE] href = ', url
                                 # If other url starting without 'http://'.
@@ -112,30 +118,30 @@ class MyHTMLParser(HTMLParser):
                             if url[0:1] == '/':
                                 # If '/' is not the final character.
                                 if self.url[7:].index('/') != (len(self.url[7:]) - 1):
-                                    if not url in MyHTMLParser.url_list:
+                                    if not url in CrawlTitlesAndUrls.url_list:
                                         print '[INFO] href = ', url
                                         with open(store_url, 'a') as writing_file:
                                             writing_file.write('http://' + self.url[7:][:self.url[7:].index('/')]
                                                                + url + '\n')
-                                        MyHTMLParser.url_list.append(url)
+                                        CrawlTitlesAndUrls.url_list.append(url)
                                     else:
                                         print '[DUPLICATE] href = ', url
                                 # If '/' is the final character.
                                 else:
-                                    if not url in MyHTMLParser.url_list:
+                                    if not url in CrawlTitlesAndUrls.url_list:
                                         print '[INFO] href = ', url
                                         with open(store_url, 'a') as writing_file:
                                             writing_file.write(self.url + url[1:] + '\n')
-                                        MyHTMLParser.url_list.append(url)
+                                        CrawlTitlesAndUrls.url_list.append(url)
                                     else:
                                         print '[DUPLICATE] href = ', url
                             # If the url is a relative path.
                             else:
-                                if not url in MyHTMLParser.url_list:
+                                if not url in CrawlTitlesAndUrls.url_list:
                                     print '[INFO] href = ', url
                                     with open(store_url, 'a') as writing_file:
                                         writing_file.write(self.url + url + '\n')
-                                    MyHTMLParser.url_list.append(url)
+                                    CrawlTitlesAndUrls.url_list.append(url)
                                 else:
                                     print '[DUPLICATE] href = ', url
 
@@ -162,7 +168,7 @@ class MyHTMLParser(HTMLParser):
                 self.found_a = False
 
     def handle_data(self, data):
-        if not DEBUG or MyHTMLParser.filter(data):
+        if DEBUG or CrawlTitlesAndUrls.filter(data):
             if not self.get_url:
                 # @formatter:off
                 if self.found_a and (
@@ -171,16 +177,15 @@ class MyHTMLParser(HTMLParser):
                    self.found_h5 or self.found_h6):
                     # Eliminate duplicated topics.
                     # @formatter:on
-                    if not data.encode('utf-8').strip() in MyHTMLParser.topic_list:
+                    if not data.encode('utf-8').strip() in CrawlTitlesAndUrls.topic_list:
                         # The "print" can only output Ascii encoded normal String
                         # ,so use "encode()" method to return Ascii normal String.
                         print '[INFO] Data = ', data.encode('utf-8').strip()
-
                         with open(store_topic, 'a') as writing_file:
                             # Use "encode()" method to return Ascii normal String
                             writing_file.write(data.encode('utf-8').strip())
                             writing_file.write('\n')
-                        MyHTMLParser.topic_list.append(data.encode('utf-8').strip())
+                        CrawlTitlesAndUrls.topic_list.append(data.encode('utf-8').strip())
                     else:
                         print '[DUPLICATE] Data = ', data.encode('utf-8').strip()
 
@@ -218,7 +223,6 @@ class MyHTMLParser(HTMLParser):
 
 def main():
     welcome_text()
-
     if DEBUG:
         urllib.urlretrieve(debug_url, debug_storepath)
 
@@ -230,13 +234,12 @@ def main():
         # with open(debug_storepath) as reading_file:
         # debug_htmltxt2 = reading_file.read()
 
-        url_parser = MyHTMLParser(False, debug_url)
+        url_parser = CrawlTitlesAndUrls(False, debug_url)
         url_parser.feed(debug_htmltxt.decode('utf-8'))
         # url_parser.feed(debug_htmltxt2.decode('utf-8'))
         url_parser.close()
-
     else:
-        # Clean the to-be-processing files.
+        # Clean the to-process files.
         open(store_url, 'w').close()
         open(store_topic, 'w').close()
 
@@ -253,7 +256,7 @@ def main():
         htmltxt = home_handler.read()
         home_handler.close()
 
-        url_parser = MyHTMLParser(True, url)
+        url_parser = CrawlTitlesAndUrls(True, url)
         # The "feed" method's parameter had better be Unicode
         # ,so use decode('utf-8') to restore to Unicode string.
         url_parser.feed(htmltxt.decode('utf-8'))
@@ -271,23 +274,22 @@ def main():
                 # "read()" return Ascii encoded normal String(htmltxt2)
                 htmltxt2 = fetch_handler.read()
 
-                # (TODO) TEMPORARY SOLUTION..
+                # (TMP) TEMPORARY SOLUTION..
                 # @formatter:off
-                # (still need to get rid of  number and(or) ';' following by the next word without any space)
+                # (TODO) Still need to get rid of  number and(or) ';' following by the next word without any space..
                 htmltxt2 = htmltxt2.replace('&#', ' ').replace(';', ' ').replace('039', ' ').replace('8217', ' ')\
                                    .replace('039', ' ').replace('8217', ' ').replace('8216', ' ').replace('39', ' ')
                 # @formatter:on
-                # (note) Parameter 'w' will overwrite the original file with a new one.
+                # (NOTE) Parameter 'w' will overwrite the original file with a new one.
                 # Store reading html text to a file.
                 with open(store_html_path, 'w') as retPage:
                     retPage.write(htmltxt2)
 
-                parser = MyHTMLParser(False, url)
+                parser = CrawlTitlesAndUrls(False, url)
                 # The "feed" method's parameter had better be Unicode
                 # ,so use decode('utf-8') to restore to Unicode string.
                 try:
                     parser.feed(htmltxt2.decode('utf-8'))
-
                 except Exception:
                     print '[ERROR] Parsing Error: ', pageUrl, ' in ', store_html_path
                     parser.feed(unicode(htmltxt2, errors='ignore'))
